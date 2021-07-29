f886dace2f4bc494a18d862d41d3547e72ffc1a0
Jovincent Del Mundo <jovincent.delmundo@gmail.com>
2021-07-28 18:02:52 -0700

    adding syntax

---
+++ app1.js
@@ -0,0 +1,9 @@
+let favoriteDino = prompt('What is your favorite Dinosaur?')
+
+if (favoriteDino.toLocaleLowerCase() === 'Tyrannosaurus Rex'){
+    prompt('good choice!'){
+        else{
+            prompt('... The Tyrannosaurus Rex is much cooler')
+        }
+    }
+}
