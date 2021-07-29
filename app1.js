081c4a0d39653ce020336d6df29f2ece42de48a5
Jovincent Del Mundo <jovincent.delmundo@gmail.com>
2021-07-29 11:50:30 -0700

    adding function syntax

--- app1.js
+++ app1.js
@@ -1,9 +1,11 @@
 let favoriteDino = prompt('What is your favorite Dinosaur?')
 
+function oneAnswer(){
 if (favoriteDino.toLowerCase() !== 'tyrannosaurus rex') {
-    alert('The Tyrannosaurus Rex is much cooler... but you can come in anyway.')
+    alert('The Tyrannosaurus Rex is much cooler... but you can come in anyway.');
 } else {
-    alert('Good choice!')
+    alert('Good choice!');
 }
+};
 
-
+oneAnswer();
